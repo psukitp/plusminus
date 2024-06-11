904ee76c445a2fa30e3cd9367a94ace181ae9a66
psukitp <jfellertv@gmail.com>
2024-06-11 22:46:05 +0500

    add fulscreen loader on register and

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react'
 import { ReviewPage } from '@components/review/review-page'
 import { Sider } from '@components/sider'
 import { ActiveCaption } from '@components/sider/types'
-import { ExpensesPage } from '@components/expenses/expenses-page'
 import { RegisterPage } from '@components/register'
 import { AuthPage } from '@components/auth'
 import { Routes, Route } from 'react-router-dom'
@@ -14,6 +13,7 @@ import { SettingsPage } from '@components/settings/SettingsPage'
 import { IncomesPageDataContainer } from '@components/incomes/incomes-page/IncomesPageDataConteiner'
 import { CategoriesPageDataContainer } from '@components/categories/categories-page/CategoriesPageDataContainer'
 import { ExpensesPageDataContainer } from '@components/expenses/expenses-page/ExpensesPageDataContainer'
+import { Loader } from '@components/common/loaders'
 
 const initialActiveCaption: ActiveCaption = {
   categories: false,
@@ -31,6 +31,7 @@ const App = () => {
   })
 
   const user = useUser(state => state.data)
+  const loading = useUser(state => state.loading)
   const { onCheck } = useAuth()
 
   useEffect(() => {
@@ -47,19 +48,30 @@ const App = () => {
 
   return (
     <div style={{ height: '100%', display: 'flex', background: 'linear-gradient(135deg, #f7f8fa 0%, #e6e7ed 100%)' }}>
-      {!!user.id && <Sider
-        activeCaption={activeCaption}
-        setActiveButton={(value: Partial<ActiveCaption>) => onChangeActiveCaption(value)} />}
-      <Routes>
-        <Route path='/auth' element={<AuthPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/review' element={<ReviewPage />} />
-        <Route path='/expenses' element={<ExpensesPageDataContainer />} />
-        <Route path='/incomes' element={<IncomesPageDataContainer />} />
-        <Route path='/categories' element={<CategoriesPageDataContainer />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/settings' element={<SettingsPage />} />
-      </Routes>
+      {loading ? (
+        <div className='fullscreen_loader'>
+          <Loader fontSize={50} />
+        </div>
+      ) : (
+        <>
+          {!!user.id && (
+            <Sider
+              activeCaption={activeCaption}
+              setActiveButton={(value: Partial<ActiveCaption>) => onChangeActiveCaption(value)}
+            />
+          )}
+          <Routes>
+            <Route path='/auth' element={<AuthPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/review' element={<ReviewPage />} />
+            <Route path='/expenses' element={<ExpensesPageDataContainer />} />
+            <Route path='/incomes' element={<IncomesPageDataContainer />} />
+            <Route path='/categories' element={<CategoriesPageDataContainer />} />
+            <Route path='/profile' element={<ProfilePage />} />
+            <Route path='/settings' element={<SettingsPage />} />
+          </Routes>
+        </>
+      )}
     </div>
   )
 }
--- src/components/common/loaders/Loader.tsx
+++ src/components/common/loaders/Loader.tsx
@@ -2,8 +2,8 @@ import { LoadingOutlined } from '@ant-design/icons';
 import { Spin } from 'antd';
 import './Loader.less'
 
-export const Loader = () => <div className='loader-container'>
-    <div className='loader-item'>
-        <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
+export const Loader = ({ fontSize }: { fontSize?: number }) => <div className='loader-container'>
+    <div className='loader-item' >
+        <Spin indicator={<LoadingOutlined style={{ fontSize: fontSize ?? 24 }} spin />} />
     </div>
 </div>
