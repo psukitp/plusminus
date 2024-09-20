bf19dac79ef71ff7a481ed3133e8f0ef25d19cff
psukitp <jfellertv@gmail.com>
2024-09-20 18:28:48 +0500

    format

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -91,7 +91,9 @@ export const getFormattedAmount = (amount: number | string) => {
   const stringAmount = amount.toString()
   const [integerPart, decimalPart] = stringAmount.split(/[.,]/)
   const formattedIntegerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+  const truncatedDecimalPart = decimalPart ? decimalPart.slice(0, 2) : ''
+
   return decimalPart
-    ? `${formattedIntegerPart},${decimalPart}`
+    ? `${formattedIntegerPart},${truncatedDecimalPart}`
     : formattedIntegerPart
 }
